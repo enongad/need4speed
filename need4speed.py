9908c5c1980d02c4559c4b0429c9da08808514ba
enongad <fabiandagnone@gmail.com>
2019-02-23 11:07:16 +0800

    create geom function and save to shp function

--- need4speed.py
+++ need4speed.py
@@ -1,7 +1,9 @@
-from wrangle import load_in_json, turn_google_location_json_into_pandas
-
+from wrangle import load_in_json, turn_google_location_json_into_pandas, turn_geo_df_into_shp, create_geometry
 
 dict_json = load_in_json('data/location_download_20190218.json')
 
-turn_google_location_json_into_pandas(dict_json)
+df = turn_google_location_json_into_pandas(dict_json)
 
+df_geom = create_geometry(df)
+
+turn_geo_df_into_shp(df_geom, file='data/locations.shp')
