97ead691d2f930a380c89cbbf77b07912d3ced65
enongad <fabiandagnone@gmail.com>
2019-03-04 20:46:23 +0800

    general running updates

--- need4speed.py
+++ need4speed.py
@@ -1,13 +1,26 @@
 from wrangle import load_in_json, turn_google_location_json_into_pandas, turn_geo_df_into_shp, create_geometry
+from spatial import get_way_points_driving
+import pandas as pd
 
-dict_json = load_in_json('data/location_download_20190218.json')
-print('data loaded')
+# dict_json = load_in_json('data/location_download_20190218.json')
+# print('data loaded')
+#
+# df = turn_google_location_json_into_pandas(dict_json)
+# print('data into pd')
+#
+# df.to_csv('data/locations_df.csv')
 
-df = turn_google_location_json_into_pandas(dict_json)
-print('data into pd')
+#
 
-df_geom = create_geometry(df)
-print('create geom')
+# df_geom = create_geometry(df)
+# print('create geom')
+#
+# turn_geo_df_into_shp(df_geom, file='data/locations.shp')
+# print('saved to shp')
 
-turn_geo_df_into_shp(df_geom, file='data/locations.shp')
-print('saved to shp')
+# get_way_points_driving(df)
+
+
+df = pd.read_csv('data/locations_df.csv')
+
+print(get_way_points_driving(df))
